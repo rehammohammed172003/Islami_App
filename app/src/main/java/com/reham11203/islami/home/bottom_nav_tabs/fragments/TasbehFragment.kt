168c8044e8a5e968df965b6935a499a4f68cd79b
rehammohammed172003 <94254158+rehammohammed172003@users.noreply.github.com>
2025-01-25 01:26:42 +0200

    + Tasbeeh Fragment (Code)

--- app/src/main/java/com/reham11203/islami/home/bottom_nav_tabs/fragments/TasbehFragment.kt
+++ app/src/main/java/com/reham11203/islami/home/bottom_nav_tabs/fragments/TasbehFragment.kt
@@ -1,21 +1,54 @@
 package com.reham11203.islami.home.bottom_nav_tabs.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.reham11203.islami.R
+import com.reham11203.islami.databinding.FragmentTasbehBinding
 
 class TasbehFragment : Fragment() {
 
+    lateinit var binding: FragmentTasbehBinding
+    lateinit var azkarList: MutableList<String>
+    private var currentIndex = 0
+    private var counter = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_tasbeh, container, false)
-        return view
+    ): View {
+        binding = FragmentTasbehBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        azkarList = resources.getStringArray(R.array.azkar_list).toMutableList()
+        initView()
+        onSebhaClick()
+    }
+
+    private fun onSebhaClick() {
+        binding.sebhaBody.setOnClickListener {
+            binding.sebhaBody.rotation += (360 / 33).toFloat()
+
+            if (counter < 33)
+                counter++
+            else {
+                counter = 0
+                currentIndex = if (currentIndex < azkarList.size - 1) ++currentIndex else 0
+                binding.zikrTv.text = azkarList[currentIndex]
+            }
+            binding.counterTv.text = "$counter"
+
+        }
+    }
+
+    private fun initView() {
+        binding.zikrTv.text = azkarList[currentIndex]
+        binding.counterTv.text = "$counter"
     }
 
 }
